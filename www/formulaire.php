b226fe63d5733f4f9081065bd4e0fa691294e745
urpagin <urpagin@domain.com>
2024-02-02 19:29:52 +0100

    he

--- www/formulaire.php
+++ www/formulaire.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Formulaire</title>
+    <!-- Sets the favicon -->
+    <link rel="icon" type="image/x-icon" href="static/logo.png">
 </head>
 
+<!-- js catches ?error parameter, if so shows a popup to the user -->
 <script>
     // Check for the error query parameter
     window.onload = function () {
@@ -22,96 +25,109 @@
 </script>
 
 <body>
+<!-- This div contains the whole page -->
+<div class="content-form">
 
-<h1 style="color: chocolate">Formulaire de préinscription au club de Handball du lycée Ferdinand BUISSON (Voiron).</h1>
-<h2 style="color: green">Veuillez compléter le formulaire pour la session 2023-2024 et vérifier ensuite les informations
-    saisies.</h2>
-
-<hr class="rounded">
-
-<form action="recapitulatif.php" method="post">
-
-    <div class="field">
-        <label for="license">Type de licence :</label>
-        <select id="license" name="license" required>
-            <option value="Joueur">Joueur</option>
-            <option value="Dirigeant">Dirigeant</option>
-            <option value="Arbitre">Arbitre</option>
-            <option value="Mutation">Mutation</option>
-        </select>
-    </div>
-
-    <div class="field">
-        <label for="categorie">Catégorie :</label>
-        <select id="categorie" name="categorie" required>
-            <option value="Moins de 13 ans">Moins de 13 ans</option>
-            <option value="Moins de 15 ans">Moins de 15 ans</option>
-            <option value="Moins de 18 ans">Moins de 18 ans</option>
-            <option value="Hand à 4">Hand à 4</option>
-            <option value="Sénior">Sénior</option>
-        </select>
-    </div>
+    <h1 style="color: chocolate">Formulaire de préinscription au club de Handball du lycée Ferdinand BUISSON (Voiron).</h1>
+    
+    <img src="static/logo.png" alt="Handball Lorem Ipsum" class="logo">
+    
+    <h2 style="color: green">Veuillez compléter le formulaire pour la session 2023-2024 et vérifier ensuite les
+        informations
+        saisies.</h2>
 
     <hr class="rounded">
 
-    <h2 style="color: green">Vos informations personelles</h2>
+    <!-- The form that will get input from the user. It is sent via a POST request -->
+    <form action="recapitulatif.php" method="post">
 
-    <div class="infos-personelles">
-
-        <p class="civility-text">Civilité</p>
-
-        <div class="field" id="civility-radio">
-            <input type="radio" id="civility1" name="civility" class="field" value="Monsieur" required>
-            <label for="civility1" id="label-civility1">Monsieur</label><br>
-            <input type="radio" id="civility2" name="civility" class="field" value="Madame">
-            <label for="civility2" id="label-civility2">Madame</label>
+        <div class="field">
+            <label for="license">Type de licence :</label>
+            <select id="license" name="license" required>
+                <option value="Joueur">Joueur</option>
+                <option value="Dirigeant">Dirigeant</option>
+                <option value="Arbitre">Arbitre</option>
+                <option value="Mutation">Mutation</option>
+            </select>
         </div>
 
         <div class="field">
-            <label for="name">Nom : (obligatoire) </label>
-            <input type="text" id="name" name="name" required>
+            <label for="category">Catégorie :</label>
+            <select id="category" name="category" required>
+                <option value="Moins de 13 ans">Moins de 13 ans</option>
+                <option value="Moins de 15 ans">Moins de 15 ans</option>
+                <option value="Moins de 18 ans">Moins de 18 ans</option>
+                <option value="Hand à 4">Hand à 4</option>
+                <option value="Sénior">Sénior</option>
+            </select>
         </div>
 
-        <div class="field">
-            <label for="surname">Prénom : (obligatoire) </label>
-            <input type="text" id="surname" name="surname" required>
+        <hr class="rounded">
+
+        <h2 style="color: green">Vos informations personelles</h2>
+        
+        <div class="infos-personelles">
+
+            <p class="civility-text">Civilité</p>
+
+            <div class="field" id="civility-radio">
+                <input type="radio" id="civility1" name="civility" class="field" value="Monsieur" required>
+                <label for="civility1" id="label-civility1">Monsieur</label><br>
+                <input type="radio" id="civility2" name="civility" class="field" value="Madame">
+                <label for="civility2" id="label-civility2">Madame</label>
+            </div>
+
+            <div class="field">
+                <label for="surname">Nom : (obligatoire) </label>
+                <input type="text" id="surname" name="surname" required>
+            </div>
+
+            <div class="field">
+                <label for="name">Prénom : (obligatoire) </label>
+                <input type="text" id="name" name="name" required>
+            </div>
+
+            <div class="field">
+                <label for="birthdate">Date de naissance : (obligatoire) </label>
+                <input type="date" id="birthdate" name="birthdate" required>
+            </div>
+
+            <div class="field">
+                <label for="email">Email :</label>
+                <input type="email" id="email" name="email" placeholder="Votre adresse mail" required>
+            </div>
+
+            <div class="field">
+                <label for="phone_number">Téléphone : (obligatoire) </label>
+                <input type="tel" id="phone_number" name="phone_number" pattern="[0-9]{10}"
+                       placeholder="Entrez votre numéro de tel" required>
+            </div>
+
+            <div class="field">
+                <label for="address">Votre adresse : (obligatoire) </label>
+                <input type="text" id="address" name="address" placeholder="Tapez ici votre adresse personelle"
+                       required>
+            </div>
+
+
         </div>
 
-        <div class="field">
-            <label for="birthdate">Date de naissance : (obligatoire) </label>
-            <input type="date" id="birthdate" name="birthdate">
+        <!-- btn to submit form to recapitulatif.php -->
+        <div>
+            <input type="submit" value="Valider">
         </div>
 
-        <div class="field">
-            <label for="email">Email :</label>
-            <input type="email" id="email" name="email" placeholder="Votre adresse mail" required>
+        <hr class="rounded">
+
+        <!-- Contact mail hyperlink at the bottom of the page -->
+        <div class="contact">
+            <a href="mailto:urpagin@proton.me?subject=Contact from Website">Contactez-moi</a>
         </div>
 
-        <div class="field">
-            <label for="phone_number">Téléphone : (obligatoire) </label>
-            <input type="tel" id="phone_number" name="phone_number" pattern="[0-9]{10}"
-                   placeholder="Entrez votre numéro de tel" required>
-        </div>
+    </form>
 
-        <div class="field">
-            <label for="address">Votre adresse : (obligatoire) </label>
-            <input type="text" id="address" name="address" placeholder="Tapez ici votre adresse personelle" required>
-        </div>
-
-
-    </div>
-
-    <div>
-        <input type="submit" value="Valider">
-    </div>
-
-    <hr class="rounded">
-
-    <div class="contact">
-        <a href="mailto:urpagin@proton.me?subject=Contact from Website">Contactez-moi</a>
-    </div>
-
-</form>
+</div>
 
 </body>
+
 </html>
--- www/recapitulatif.php
+++ www/recapitulatif.php
@@ -1,4 +1,4 @@
-<html lang="">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -18,16 +18,20 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
 ?>
 
 <body>
-<h1>Votre inscription</h1>
-<h2>
-    ********************************************************************************
-    <br><br>vos coordonées<br><br>
-    ********************************************************************************
-</h2>
 
-<p>
+<!-- This div contains the whole page -->
+<div class="content-summary">
+
+    <h1 class="title">Votre inscription</h1>
+
     <?php
+    $stars = "********************************************************************************"; // 80 stars
+
+    echo "<h2 class='sub-title'>$stars<br><br>VOS COORDONÉES<br><br>$stars</h2>";
+
+    // reads form from $_POST
     $license = htmlspecialchars($_POST['license']);
+    $category = htmlspecialchars($_POST['category']);
     $civility = htmlspecialchars($_POST['civility']);
     $name = htmlspecialchars($_POST['name']);
     $surname = htmlspecialchars($_POST['surname']);
@@ -36,30 +40,38 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
     $phone_number = htmlspecialchars($_POST['phone_number']);
     $address = htmlspecialchars($_POST['address']);
 
+    // converts name to lowercase with the first letter uppercase and surname to uppercase
+    $name = ucfirst(strtolower($name));
+    $surname = strtoupper($surname);
+
+    // converts email to lowercase as it's not case-sensitive
+    $email = strtolower($email);
+
     // Check if any of the required fields are empty
-    if (empty($civility) || empty($name) || empty($surname) || empty($birthdate) || empty($email) || empty($phone_number) || empty($address)) {
+    if (empty($civility) || empty($category) || empty($name) || empty($surname) || empty($birthdate) || empty($email) || empty($phone_number) || empty($address)) {
         // Redirect to another page with an error query parameter
         header('Location: formulaire.php?error=empty');
         exit;
     }
 
-    // Echo the variables
-    echo "License: " . $license . "<br>";
-    echo "Civility: " . $civility . "<br>";
-    echo "Name: " . $name . "<br>";
-    echo "Surname: " . $surname . "<br>";
-    echo "Birthdate: " . $birthdate . "<br>";
-    echo "Email: " . $email . "<br>";
-    echo "Phone Number: " . $phone_number . "<br>"; // Corrected variable for phone number
-    echo "Address: " . $address . "<br>";
 
+    // Summarises the information to the user
+    echo "<p>$civility<br><b>$surname $name</b><br><br><br></p>";
+    echo "<p>résidant à <br><b>$address</b><br><br></p>";
+    echo "<p>Votre numéro de téléphone est le<br><b>$phone_number</b><br><br></p>";
+    echo "<p>et votre email :<br><b>$email</b></p>";
+    echo "<p>Vous êtes né le <b>$birthdate</b></p>";
 
+    echo "<h2 class='sub-title'>$stars<br><br>VOTRE CHOIX D'INSCRIPTION<br><br>$stars</h2>";
+
+    echo "<p>Vous avez choisi la license <b>$license</b></p>";
+    echo "<p>et vous avez choisi la catégorie <b>$category</b><br><br></p>";
+
+    // Offers the user the choice to redo the form - the js part allows the form data to stay which is good quality of life
+    echo "<p>Pour rectifier les informations saisies, <a href='javascript:history.back()'><b>Cliquez Ici</b></a> pour revenir à la page précédente</p>";
 
     ?>
-</p>
-
-<button onclick="location.href='formulaire.php';">Go to Target Page</button>
-
+</div>
 
 </body>
 </html>
